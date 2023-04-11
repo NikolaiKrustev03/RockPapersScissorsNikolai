fabb3d317892e10322e0e68f5471109ccd4c586a
NikolaiKrustev03 <n.krastev2003@gmail.com>
2023-04-11 17:27:16 +0300

    Optimised the game
    Made a prompt that asks the user if they want to have another go at the game

--- rock_papers_scissors.py
+++ rock_papers_scissors.py
@@ -1,34 +1,53 @@
 import random
-
 ai_input=["rock","papers","scissors"]
-pc_won="Computer won"
-p_won="Player won"
+pc_won="Computer won\n"
+p_won="Player won\n"
+def game():
+    while True:
+        try:
+            player_input=input("rock, papers, scissors?\n").lower()
+            if player_input in ai_input:
+                break
+            else:
+                raise Exception("Invalid input, please input rock, papers or scissors only")
+        except Exception as err:
+            print(err)
+    ai_move=random.choice(ai_input)
+    print(f"Computer chose: {ai_move}")
+    print(f"Player chose: {player_input}")
+    if player_input==ai_move:
+        print("It's a tie\n")
+    elif player_input=="rock":
+        if ai_move=="scissors":
+            print(p_won)
+        else:
+            print(pc_won)
+    elif player_input=="papers":
+        if ai_move=="rock":
+            print(p_won)
+        else:
+            print(pc_won)
+    else:
+        if ai_move=="rock":
+            print(pc_won)
+        else:
+            print(p_won)
+
+game()
+
 while True:
     try:
-        player_input=input("rock, papers, scissors?")
-        if player_input==ai_input[0] or player_input==ai_input[1] or player_input==ai_input[2]:
+
+        continue_game = input("Would you like to continue? (Y/N)\n")
+        if continue_game=="Y":
+            game()
+        elif continue_game=="N":
+            print("Maybe you'd like to check out my other projects on https://github.com/NikolaiKrustev03")
             break
+        elif not continue_game.isalpha():
+            print(f"{continue_game} isn't even an alphabetic character")
         else:
-            raise Exception("Invalid input, please input rock, papers or scissors only")
-    except Exception as err:
-        print(err)
-ai_move=random.choice(ai_input)
-print(f"Computer chose {ai_move}")
-print(f"Player chose {player_input}")
-if player_input==ai_move:
-    print("It's a tie")
-elif player_input=="rock":
-    if ai_move=="scissors":
-        print(p_won)
-    else:
-        print(pc_won)
-elif player_input=="papers":
-    if ai_move=="rock":
-        print(p_won)
-    else:
-        print(pc_won)
-else:
-    if ai_move=="rock":
-        print(pc_won)
-    else:
-        print(p_won)
+            print("Only Y or N as a valid answer")
+
+    except ValueError:
+        print("Got a value Error")
